0514bba675c7f135291ddd631c8ca66ac34fcd72
azacoder <85455253+azacoder@users.noreply.github.com>
2021-06-07 14:19:19 +0500

    clock

---
+++ js/scrirt.js
@@ -0,0 +1,22 @@
+
+//часы
+function clock(){
+    var date = new Date(),
+           hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
+           minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
+           seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+  }
+  setInterval(clock, 1000);
+  clock();
+
+  
+
+  //дата
+  function dateFunc() {
+    let yearValue = new Date ();
+  document.getElementById('timedate').innerHTML = yearValue.toLocaleDateString();
+  
+}
+setInterval(dateFunc,1000); 
+dateFunc();
